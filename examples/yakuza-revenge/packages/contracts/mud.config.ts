f6dc5a386b4c71834eba0f1a284e4c40300a5353
0xshora <112358132134.fibon@gmail.com>
2024-04-17 17:30:57 +0100

    feat: update YakuzaRevengeSystem

--- examples/yakuza-revenge/packages/contracts/mud.config.ts
+++ examples/yakuza-revenge/packages/contracts/mud.config.ts
@@ -36,8 +36,23 @@ export default mudConfig({
       valueSchema: {
         base: "bool",
       }
-    }
+    },
 
     /* --------------------------------- Common --------------------------------- */
+    Position: {
+      keySchema: { entity: "bytes32" },
+      valueSchema: {
+        x: "int32",
+        y: "int32",
+        parent: "bytes32",
+      },
+    },
+
+    OwnedBy: {
+      keySchema: { entity: "bytes32" },
+      valueSchema: {
+        value: "bytes32",
+      },
+    },
   },
 });
--- examples/yakuza-revenge/packages/contracts/src/systems/YakuzaRevengeSystem.sol
+++ examples/yakuza-revenge/packages/contracts/src/systems/YakuzaRevengeSystem.sol
@@ -7,20 +7,20 @@
 pragma solidity >=0.8.21;
 
 import { System } from "@latticexyz/world/src/System.sol";
-import { PositionData } from "../codegen/index.sol";
-import { UpgradeBounty } from "../codegen/index.sol";
-import { OwnedBy } from "../codegen/index.sol";
+// import { PositionData } from "../codegen/index.sol";
+// import { UpgradeBounty } from "../codegen/index.sol";
+// import { OwnedBy } from "../codegen/index.sol";
 import { IWorld } from "../codegen/world/IWorld.sol";
-import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
+// import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
 import { WorldResourceIdLib, ROOT_NAMESPACE } from "@latticexyz/world/src/WorldResourceId.sol";
 import { RESOURCE_SYSTEM } from "@latticexyz/world/src/worldResourceTypes.sol";
 import { IWorld as IPrimodiumWorld } from "../primodium-codegen/world/IWorld.sol";
 import { LibHelpers } from "../libraries/LibHelpers.sol";
-import { BuildingTileKey } from "../libraries/Keys.sol";
+// import { BuildingTileKey } from "../libraries/Keys.sol";
 
-import { ResourceCount } from "../codegen/index.sol";
-import { EResource } from "../codegen/common.sol";
-import {IFleetTransSystem} from "../primodium-codegen/world/IFleetMoveSystem.sol";
+// import { ResourceCount } from "../codegen/index.sol";
+import { EResource } from "../primodium-codegen/common.sol";
+import {IFleetMoveSystem} from "../primodium-codegen/world/IFleetMoveSystem.sol";
 
 /**
  * @dev A contract that handles upgrade bounties for buildings in a world system.
@@ -29,12 +29,11 @@ import {IFleetTransSystem} from "../primodium-codegen/world/IFleetMoveSystem.sol
  * @notice Technically Alice can issue an upgrade bounty at Bob's building, and Bob can claim it
  */
 contract YakuzaRevengeSystem is System {
-  
-  
+
   /**
    * @dev Send the resouce from orbit. On success if enough iron. Then, set expiry.
    */
-  function sendResources(bytes32 FleetID, bytes32 yakuzaAsteroidID, uint256 resourceValue) {
+  function sendResources(bytes32 fleetID, bytes32 yakuzaAsteroidID, uint256 resourceValue) external{
 
     // EResource.Iron;
 
@@ -51,13 +50,13 @@ contract YakuzaRevengeSystem is System {
 
 
     // check if the player has enough iron
-    require(IWorld(_world).Primodium_PrimodiumSystem_ResourceCount.get(playerEntity, EResource.Iron) > resourceValue, "You don't have enough iron");
+    // require(IWorld(_world).Primodium_PrimodiumSystem_ResourceCount.get(playerEntity, EResource.Iron) > resourceValue, "You don't have enough iron");
 
 
     // check yakuzaAsteroidID is owned by no one or yakuza contract.
-    if (YakuzaAsteroid.get(yakuzaAsteroidID) == false) {
+    if (IsYakuzaAsteroid.get(yakuzaAsteroidID) == false) {
       // check if the asteroid is free
-      require(IsFleetEmpty(yakuzaAsteroidID), "That asteroid is not empty");
+      // require(IsFleetEmpty(yakuzaAsteroidID), "That asteroid is not empty");
       
       // set the asteroid to be owned by the yakuza
       IsYakuzaAsteroid.set(yakuzaAsteroidID, true);
@@ -71,7 +70,7 @@ contract YakuzaRevengeSystem is System {
     resourceCounts[1] = resourceValue; // 二つ目のリソースのカウント
     
     // transfer the resources from the player to the yakuzaAsteroid
-    require(IFleetTransSystem.transferResourcesFromFleetToSpaceRock(FleetID, yakuzaAsteroidID, resourceCounts),
+    require(IFleetMoveSystem.transferResourcesFromFleetToSpaceRock(fleetID, yakuzaAsteroidID, resourceCounts),
       "Failed to transfer resources from fleet to space rock");
 
     uint64 expiry = block.timestamp + 1 days * (resourceValue);
